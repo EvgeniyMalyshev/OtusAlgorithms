d5887a3d20075b5daba8963fb4602d415c49532b
sleepyowly <sogol@bk.ru>
2021-12-05 13:17:04 +0300

    тест по заполнению и очистке массивов Время заполнения single array PT0.000083S Время заполнения factor array PT0.000035S Время заполнения matrix array PT0.000018S Время заполнения vector array PT0.000016S Время очистки single array PT0.000015S Время очистки factor array PT0.000013S Время очистки matrix array PT0.000014S Время очистки vector array PT0.000012S

--- src/test/java/Arrays.java
+++ src/test/java/Arrays.java
@@ -1,11 +1,92 @@
+import arrays.array.*;
 import org.junit.Test;
+
+import java.time.ZonedDateTime;
 
 public class Arrays {
 
 
     @Test
-    public void testArray(){
+    public void testArray() {
+        System.out.println(singleArrayTestFill());
+        System.out.println(factorArrayTestFill());
+        System.out.println(matrixArrayTestFill());
+        System.out.println(vectorArrayTestFill());
 
+        System.out.println(singleArrayTestClean());
+        System.out.println(factorArrayTestClean());
+        System.out.println(matrixArrayTestClean());
+        System.out.println(vectorArrayTestClean());
+    }
+
+    private String singleArrayTestFill() {
+        DynamicArray<Integer> array = new SingleArray<>();
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        fillArray(array, 100);
+        return TimeCheck.endTrackTime(before, "Время заполнения single array");
+    }
+
+    private String singleArrayTestClean() {
+        DynamicArray<Integer> array = new SingleArray<>();
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        cleanArray(array);
+        return TimeCheck.endTrackTime(before, "Время очистки single array");
+    }
+
+    private String factorArrayTestFill() {
+        DynamicArray<Integer> array = new FactorArray<>();
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        fillArray(array, 100);
+        return TimeCheck.endTrackTime(before, "Время заполнения factor array");
+    }
+
+    private String factorArrayTestClean() {
+        DynamicArray<Integer> array = new FactorArray<>();
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        cleanArray(array);
+        return TimeCheck.endTrackTime(before, "Время очистки factor array");
+    }
+
+    private String matrixArrayTestFill() {
+        DynamicArray<Integer> array = new MatrixArray<>();
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        fillArray(array, 100);
+        return TimeCheck.endTrackTime(before, "Время заполнения matrix array");
+    }
+
+    private String matrixArrayTestClean() {
+        DynamicArray<Integer> array = new MatrixArray<>();
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        cleanArray(array);
+        return TimeCheck.endTrackTime(before, "Время очистки matrix array");
+    }
+
+    private String vectorArrayTestFill() {
+        DynamicArray<Integer> array = new VectorArray<>();
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        fillArray(array, 100);
+        return TimeCheck.endTrackTime(before, "Время заполнения vector array");
+    }
+
+    private String vectorArrayTestClean() {
+        DynamicArray<Integer> array = new VectorArray<>();
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        cleanArray(array);
+        return TimeCheck.endTrackTime(before, "Время очистки vector array");
+    }
+
+    private DynamicArray<Integer> fillArray(DynamicArray<Integer> integerDynamicArray, int number) {
+        for (int i = 0; i < integerDynamicArray.size(); i++) {
+            integerDynamicArray.add(number, i);
+        }
+        return integerDynamicArray;
+    }
+
+    private DynamicArray<Integer> cleanArray(DynamicArray<Integer> integerDynamicArray) {
+        for (int i = 0; i < integerDynamicArray.size(); i++) {
+            integerDynamicArray.remove(i);
+        }
+        return integerDynamicArray;
     }
 
 
