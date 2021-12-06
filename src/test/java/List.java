646e94c6a8e7063d4aa7e536a73d98429b3ff756
sleepyowly <sogol@bk.ru>
2021-12-06 10:50:54 +0300

    Тест листа и очереди

--- src/test/java/List.java
+++ src/test/java/List.java
@@ -1,2 +1,22 @@
+import arrays.list.FactorArrayList;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class List {
+
+    private final FactorArrayList<Integer> factorArrayList = new FactorArrayList<>();
+
+    @Test
+    public void testList() {
+        factorArrayList.add(1);
+        factorArrayList.add(2);
+        factorArrayList.add(3);
+        factorArrayList.add(4);
+        factorArrayList.add(5);
+        Assert.assertEquals(5, factorArrayList.size());
+        Assert.assertEquals(Integer.valueOf(5), factorArrayList.get(factorArrayList.size() - 1));
+        Assert.assertEquals(Integer.valueOf(3), factorArrayList.get(2));
+        Assert.assertEquals(Integer.valueOf(3), factorArrayList.remove(2));
+        Assert.assertEquals(4, factorArrayList.size());
+    }
 }
--- src/test/java/Queue.java
+++ src/test/java/Queue.java
@@ -1,2 +1,21 @@
+import arrays.queue.PriorityQueueImpl;
+import org.junit.Assert;
+import org.junit.Test;
+
+
 public class Queue {
+    private final PriorityQueueImpl<Integer> priorityQueue = new PriorityQueueImpl<>();
+
+    @Test
+    public void testQueue() {
+        priorityQueue.enqueue(2, 1);
+        priorityQueue.enqueue(-1, 2);
+        priorityQueue.enqueue(1, 3);
+        priorityQueue.enqueue(3, 4);
+        priorityQueue.enqueue(-2, 5);
+
+        Assert.assertEquals(Integer.valueOf(4), priorityQueue.dequeue());
+        Assert.assertEquals(Integer.valueOf(1), priorityQueue.dequeue());
+        Assert.assertEquals(Integer.valueOf(3), priorityQueue.dequeue());
+    }
 }
