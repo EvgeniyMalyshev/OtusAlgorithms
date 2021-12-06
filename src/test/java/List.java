391364b8e1ed4b692f49fe3c40cc48deb8a5d7d6
sleepyowly <sogol@bk.ru>
2021-12-06 11:02:28 +0300

    Дополнил List Время заполнения list PT0.000089S

--- src/test/java/List.java
+++ src/test/java/List.java
@@ -1,6 +1,9 @@
+
 import arrays.list.FactorArrayList;
 import org.junit.Assert;
 import org.junit.Test;
+
+import java.time.ZonedDateTime;
 
 public class List {
 
@@ -18,5 +21,15 @@ public class List {
         Assert.assertEquals(Integer.valueOf(3), factorArrayList.get(2));
         Assert.assertEquals(Integer.valueOf(3), factorArrayList.remove(2));
         Assert.assertEquals(4, factorArrayList.size());
+
+        System.out.println(fillList(factorArrayList, 16));
+    }
+
+    private String fillList(FactorArrayList<Integer> factorArrayList, int number) {
+        ZonedDateTime before = TimeCheck.startTrackTime();
+        for (int i = 0; i < number; i++) {
+            factorArrayList.add(i);
+        }
+        return TimeCheck.endTrackTime(before, "Время заполнения list");
     }
 }
